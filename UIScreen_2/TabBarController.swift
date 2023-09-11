321c6bd3797535d62a88ef2474656829db7b5817
Naz <nazerke.sembay11@mail.ru>
2023-09-11 22:04:11 +0600

    TabBar controller added

--- UIScreen_2/TabBarController.swift
+++ UIScreen_2/TabBarController.swift
@@ -8,10 +8,61 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        generateTabBar()
+        setTabBarAppearance()
     }
-
+    
+    private func generateTabBar() {
+        viewControllers = [
+            generateVC(
+                viewController: MainViewController(),
+                title: "Главная",
+                image: UIImage(named: "main")
+            ),
+            
+            generateVC(
+                viewController: ReportsViewController(),
+                title: "Отчеты",
+                image: UIImage(named: "reports")
+            ),
+            
+            generateVC(
+                viewController: ProfileViewController(),
+                title: "Профиль",
+                image: UIImage(named: "profile")
+            )
+        ]
+    }
+    
+    private func generateVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.image = image
+        viewController.tabBarController?.title = title
+        return viewController
+    }
+    
+    private func setTabBarAppearance() {
+        tabBar.tintColor = .black
+        tabBar.alpha = 1
+        tabBar.items?[0].selectedImage = UIImage(named: "mainSelected")?.withRenderingMode(.alwaysOriginal)
+        
+        let lineView = UIView()
+        lineView.backgroundColor = UIColor(
+            red: 185/255,
+            green: 193/255,
+            blue: 217/255,
+            alpha: 0.5
+        )
+        lineView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: tabBar.frame.size.width,
+            height: 1
+        )
+        
+        tabBar.addSubview(lineView)
+    }
 }
+
